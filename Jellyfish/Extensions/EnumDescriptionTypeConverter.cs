d60a61d1b2767346e852b3c36f09cf53890f1058
mrousavy <mrousavy@donotreply.github.com>
2018-05-13 21:05:35 +0200

    Add enum binding extension

---
+++ Jellyfish/Extensions/EnumDescriptionTypeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Jellyfish.Extensions
+{
+    // Source: http://brianlagunas.com/a-better-way-to-data-bind-enums-in-wpf/
+    public class EnumDescriptionTypeConverter : EnumConverter
+    {
+        public EnumDescriptionTypeConverter(Type type)
+            : base(type)
+        {
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType != typeof(string)) return base.ConvertTo(context, culture, value, destinationType);
+            if (value == null) return string.Empty;
+            var fi = value.GetType().GetField(value.ToString());
+            if (fi == null) return string.Empty;
+            var attributes = (DescriptionAttribute[]) fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return ((attributes.Length > 0) && (!string.IsNullOrEmpty(attributes[0].Description))) ? attributes[0].Description : value.ToString();
+
+        }
+    }
+}
