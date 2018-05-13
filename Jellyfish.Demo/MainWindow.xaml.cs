94033d7198c9c44bce01ee62e3562ced16a452de
mrousavy <mrousavy@donotreply.github.com>
2018-05-13 13:17:53 +0200

    Add demo WPF project

---
+++ Jellyfish.Demo/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Jellyfish.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly Random _random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var timer = new Timer(TimerCallback);
+        }
+
+        private void TimerCallback(object state)
+        {
+            int random = _random.Next(1);
+            TestProperty = random == 0 ? "zero" : "one";
+        }
+
+        [Property]
+        public string TestProperty { get; set; }
+    }
+}
