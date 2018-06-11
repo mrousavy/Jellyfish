4fa4ea88e7eafa1cc8f3415bed7e7b7668529a48
mrousavy <mrousavy@donotreply.github.com>
2018-06-11 20:19:06 +0200

    Update AssemblyInfo

--- Jellyfish/Properties/AssemblyInfo.cs
+++ Jellyfish/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Jellyfish")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An incredibly light and type safe MVVM library for .NET WPF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mrousavy")]
 [assembly: AssemblyProduct("Jellyfish")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.2")]
+[assembly: AssemblyFileVersion("1.0.1.2")]
