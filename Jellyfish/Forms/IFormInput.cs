d5634c5fda7117f941536d1c36f0c6d566525351
Marc Rousavy <marcrousavy@hotmail.com>
2018-06-12 13:56:33 +0200

    Define IFormInput interface

---
+++ Jellyfish/Forms/IFormInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Jellyfish.Forms
+{
+    /// <summary>
+    ///     Any user input form's input field
+    /// </summary>
+    public interface IFormInput<T>
+    {
+        /// <summary>
+        ///     The text or question to display next to the input field
+        /// </summary>
+        string Text { get; set; }
+        /// <summary>
+        ///     The formatter to use for custom formatting the field
+        /// </summary>
+        /// <example>
+        ///     You can define a formatter to display hours like this:
+        /// 
+        ///     var formatter = val => $"{val}h";
+        /// </example>
+        Func<T, T> Formatter { get; set; }
+    }
+}
