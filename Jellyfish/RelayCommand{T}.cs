c5657c4f9d488b754e486f638e0b73f3ffce7f71
mrousavy <mrousavy@donotreply.github.com>
2018-06-14 20:47:14 +0200

    Separate RelayCommand and RelayCommand<T>

--- Jellyfish/RelayCommand{T}.cs
+++ Jellyfish/RelayCommand{T}.cs
@@ -5,30 +5,31 @@ namespace Jellyfish
 {
     /// <inheritdoc />
     /// <summary>
-    ///     An <see cref="T:System.Windows.Input.ICommand" /> implementation
+    ///     A type safe <see cref="T:System.Windows.Input.ICommand" /> implementation
     /// </summary>
-    public class RelayCommand : ICommand
+    /// <typeparam name="T">The type of the parameter</typeparam>
+    public class RelayCommand<T> : ICommand
     {
-        private readonly Func<object, bool> _canExecute;
-        private readonly Action<object> _execute;
+        private readonly Func<T, bool> _canExecute;
+        private readonly Action<T> _execute;
 
         /// <summary>
         ///     Initialize a new instance of the <see cref="RelayCommand{T}" />, a <see cref="ICommand" /> implementation,
         ///     where <see cref="CanExecute" /> is always true
         /// </summary>
         /// <param name="execute">The callback to execute on command execution</param>
-        public RelayCommand(Action<object> execute)
+        public RelayCommand(Action<T> execute)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = arg => true;
         }
 
         /// <summary>
-        ///     Initialize a new instance of the <see cref="RelayCommand{T}" />, an <see cref="ICommand" /> implementation
+        ///     Initialize a new instance of the <see cref="RelayCommand{T}" />, a <see cref="ICommand" /> implementation
         /// </summary>
         /// <param name="execute">The callback to execute on command execution</param>
         /// <param name="canExecute">The callback to check if this command can execute</param>
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
@@ -42,14 +43,23 @@ namespace Jellyfish
         /// </summary>
         /// <param name="o">The parameter object</param>
         /// <returns>If this command can execute with this parameter</returns>
-        public bool CanExecute(object o) => _canExecute?.Invoke(o) ?? false;
+        public bool CanExecute(object o)
+        {
+            if (o is T parameter)
+                return _canExecute?.Invoke(parameter) ?? false;
+            return false;
+        }
 
         /// <inheritdoc />
         /// <summary>
         ///     Execute the command with the given parameter
         /// </summary>
         /// <param name="o">The parameter object</param>
-        public void Execute(object o) => _execute?.Invoke(o);
+        public void Execute(object o)
+        {
+            if (o is T parameter)
+                _execute?.Invoke(parameter);
+        }
 
         /// <summary>
         ///     Raise the <see cref="CanExecuteChanged" /> event
