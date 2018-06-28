f69d5e25614b94d4664df786a31d5721e8fb6ecf
mrousavy <mrousavy@donotreply.github.com>
2018-06-28 19:45:35 +0200

    Add ChannelFor<T> func to Model

--- Jellyfish/Model.cs
+++ Jellyfish/Model.cs
@@ -1,14 +1,15 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace Jellyfish
+﻿namespace Jellyfish
 {
     /// <summary>
-    ///     The base class of any Model in the MVVM pattern
+    ///     The base class of any Model in the MVVM pattern providing boilerplate Model code
     /// </summary>
     public abstract class Model
     {
-        protected Model([CallerMemberName] string callerName = "")
-        {
-        }
+        /// <summary>
+        ///     Get the channel for the given type `<see cref="TMessage"/>`
+        /// </summary>
+        /// <typeparam name="TMessage">The type of the messages this channel handles</typeparam>
+        /// <returns>The opened message channel</returns>
+        public MessageChannel<TMessage> ChannelFor<TMessage>() => MessageChannel<TMessage>.Channel;
     }
 }
--- Jellyfish/ViewModel.cs
+++ Jellyfish/ViewModel.cs
@@ -1,15 +1,10 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace Jellyfish
+﻿namespace Jellyfish
 {
     /// <inheritdoc />
     /// <summary>
-    ///     The base class of any View-Model in the MVVM pattern
+    ///     The base class of any View-Model in the MVVM pattern providing boilerplate View-Model code (inherits from <see cref="ObservableObject"/>)
     /// </summary>
     public abstract class ViewModel : ObservableObject
     {
-        protected ViewModel([CallerMemberName] string callerName = "")
-        {
-        }
     }
 }
