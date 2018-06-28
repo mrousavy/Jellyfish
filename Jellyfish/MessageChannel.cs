b2ed0e8ea916ca3fc4f7ba9ec3b006a2b401d07d
mrousavy <mrousavy@donotreply.github.com>
2018-06-28 15:09:37 +0200

    Implement the MessageChannel
    The MessageChannel<T> allows sending and receiving objects inside the application

---
+++ Jellyfish/MessageChannel.cs
@@ -0,0 +1,36 @@
+﻿namespace Jellyfish
+{
+    /// <summary>
+    ///     An event based channel in the message network, observable for notifications by receivers. There is a channel for each type `<see cref="TMessage"/>`
+    /// </summary>
+    /// <typeparam name="TMessage">The type of the messages this channel handles</typeparam>
+    public class MessageChannel<TMessage>
+    {
+        private static MessageChannel<TMessage> _instance;
+
+        /// <summary>
+        ///     Get the channel for the given type `<see cref="TMessage"/>`
+        /// </summary>
+        public static MessageChannel<TMessage> Channel => _instance ?? (_instance = new MessageChannel<TMessage>());
+
+        /// <summary>
+        ///     Notify all <see cref="MessageReceived"/> subscribers in this channel with the given <see cref="message"/>
+        /// </summary>
+        /// <param name="message">The message to notify all subscribers about</param>
+        public void Notify(TMessage message)
+        {
+            MessageReceived?.Invoke(message);
+        }
+
+        /// <summary>
+        ///     A method delegate representing a message received event handler
+        /// </summary>
+        /// <param name="message">The message that was received by the sender</param>
+        public delegate void MessageReceivedHandler(TMessage message);
+
+        /// <summary>
+        ///     The handleable event for messages received from a sender's <see cref="Notify"/> call
+        /// </summary>
+        public event MessageReceivedHandler MessageReceived;
+    }
+}
