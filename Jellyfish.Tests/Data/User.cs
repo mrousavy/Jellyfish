d320d5c9d86720c08682270bfb7f0863919e73a5
mrousavy <mrousavy@donotreply.github.com>
2018-06-29 18:48:45 +0200

    Add unit tests for observable objects

---
+++ Jellyfish.Tests/Data/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Jellyfish.Tests.Data
+{
+    public class User : ObservableObject
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+    }
+}
---
+++ Jellyfish.Tests/TestObservableObjects.cs
@@ -0,0 +1,59 @@
+﻿using Jellyfish.Tests.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jellyfish.Tests
+{
+
+    [TestClass]
+    public class TestObservableObjects
+    {
+        public User User { get; private set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            User = new User();
+        }
+
+        [TestMethod]
+        public void TestPropertyChangedRaised()
+        {
+            int raisedCount = 0;
+            // set `raised` to true on property changed
+            User.PropertyChanged += (s, a) => { raisedCount++; };
+            // change a property so effectively raise property changed event
+            User.Name = "Marc";
+
+            // not more than 1 and not less than 1
+            Assert.IsTrue(raisedCount == 1);
+        }
+
+        [TestMethod]
+        public void TestPropertyDoesntRaiseOnSameValue()
+        {
+            int raisedCount = 0;
+            // set `raised` to true on property changed
+            User.PropertyChanged += (s, a) => { raisedCount++; };
+            // assign the property twice
+            const string value = "Test";
+            User.Name = value;
+            User.Name = value;
+
+            // not more than 1 and not less than 1
+            Assert.IsTrue(raisedCount == 1);
+        }
+
+        [TestMethod]
+        public void TestPropertyAcceptsNull()
+        {
+            int raisedCount = 0;
+            // set `raised` to true on property changed
+            User.PropertyChanged += (s, a) => { raisedCount++; };
+            // change a property to null so effectively raise property changed event
+            User.Name = null;
+
+            // not more than 1 and not less than 1
+            Assert.IsTrue(raisedCount == 1);
+        }
+    }
+}
