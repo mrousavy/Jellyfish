747569b86fb48cfdd1a7657e7bb73dafd31fadef
mrousavy <mrousavy@donotreply.github.com>
2018-07-02 23:35:58 +0200

    Add ToObservableCollection extension method

---
+++ Jellyfish/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Jellyfish.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        ///     Convert this <see cref="IEnumerable{T}"/> into an <see cref="ObservableCollection{T}"/>
+        /// </summary>
+        /// <typeparam name="T">The type of the lists</typeparam>
+        /// <param name="enumerable">The enumerable itself</param>
+        /// <returns>A newly created observable collection</returns>
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> enumerable)
+        {
+            return new ObservableCollection<T>(enumerable);
+        }
+    }
+}
