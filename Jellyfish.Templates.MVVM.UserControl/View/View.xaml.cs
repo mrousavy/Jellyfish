1d6eb4b32a750a08b830647307d2f6c25f7e078b
mrousavy <mrousavy@donotreply.github.com>
2018-08-21 12:09:17 +0200

    Add UserControl Template

---
+++ Jellyfish.Templates.MVVM.UserControl/View/View.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace $rootnamespace$
+{
+    /// <summary>
+    ///     Interaction logic for $safeitemrootname$.xaml
+    /// </summary>
+    public partial class $safeitemrootname$
+    {
+        public $safeitemrootname$()
+        {
+            InitializeComponent();
+        }
+    }
+}
